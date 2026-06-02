7d1cbc5fc6ee3ce737447e0bac458ddc8ea7265a
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-06-02 13:13:25 -0700

    fixed schema url error

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -3,6 +3,10 @@ import { PrismaClient } from "@prisma/client"
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ["error"] })
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+    datasourceUrl: process.env.DATABASE_URL,
+  })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
