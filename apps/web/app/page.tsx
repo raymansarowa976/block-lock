b32a33b97da40af5e7a594eddfb2fa3ab78277a3
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-06-04 23:01:48 -0700

    visiting site auto redirects to login

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="text-4xl font-semibold tracking-tight text-white">
-        Coming Soon
-      </h1>
-    </div>
-  );
+  redirect("/login")
 }
